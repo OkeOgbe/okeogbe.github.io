29017e5665bf9d23692d94f288f5a96e0acda569
OkeOgbe <46296469+OkeOgbe@users.noreply.github.com>
2019-04-23 00:24:48 +0100

    form fix

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let side = document.getElementById("men")
+document.getElementById("toggle").addEventListener("click", function(){
+	side.classList.toggle("active")
+});
+document.getElementById("close").addEventListener("click", function(){
+	side.classList.toggle("active")
+});
