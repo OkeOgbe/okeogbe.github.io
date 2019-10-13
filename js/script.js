7934b9d99ff5727da4d160a499aa208d4e2f02d6
OkeOgbe <46296469+OkeOgbe@users.noreply.github.com>
2019-10-13 10:46:14 +0100

    update

--- js/script.js
+++ js/script.js
@@ -18,3 +18,26 @@ setTimeout(
         figma.style.color = "#25557c";
     },1000
 );
+
+$( document ).ready( function( ) {
+	$( '.owl-carousel' ).owlCarousel( {
+		loop: true,
+		margin: 0,
+		nav: false,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		animateOut: 'fadeOut',
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 3
+			},
+			600: {
+				items: 2
+			},
+			1000: {
+				items: 1
+			}
+		}
+	} )
+} );
